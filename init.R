9f5146365b878e75e8daee6588c1ee7a9138b6cf
Nick Waters <nickp60@gmail.com>
2019-05-29 15:35:37 +0100

    try adding heoku reqs

---
+++ init.R
@@ -0,0 +1,17 @@
+#!/usr/bin/Rscript
+# Use script to run the Chordomics Shiny app (installing any dependencies along the way)
+
+if(!"devtools" %in% rownames(installed.packages())) {
+  print("Installing devtools...")
+  install.packages("devtools", repos='http://cran.us.r-project.org', dependencies = T)
+}
+
+if(!"chorddiag" %in% rownames(installed.packages())) {
+  print("Installing chorddiag...")
+  devtools::install_github("KevinMcDonnell6/chorddiag", dependencies = T)
+}
+if(!"chordomics" %in% rownames(installed.packages())) {
+  print("Installing chordomics")
+  devtools::install_github("KevinMcDonnell6/chordomics", dependencies = T)
+}
+chordomics::launchApp()
